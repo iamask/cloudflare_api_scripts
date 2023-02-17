9d2319d7f321d31c471fd31731c64c7ac273cfe5
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-02-17 18:00:51 +0800

    added comments

--- bulk-filters-delete-api.py
+++ bulk-filters-delete-api.py
@@ -20,34 +20,37 @@ response = requests.get(BASE_URL, headers=headers)
 
 # Parse the response as JSON
 data = response.json()
-#print(data)
+
 # Iterate over the data from the first API
 for zone_ids in data["result"]:
-   # print(zone_ids)
+
     # Get the ID from the current item
     zone_id = zone_ids["id"]
-  #  print(zone_id)
+
     # Make a request to the second API endpoint for the current ID
     page = 1
     while True:
+        # Get filters
         filters_url = BASE_URL + \
             f"/{zone_id}/filters?page={page}&per_page=1000"
         response = requests.get(filters_url, headers=headers)
-        data = response.json()
-        # Iterate over the data from the current page of the second API
-        for filters in data["result"]:
-         #   print(firewall_rule_ids)
-            # Check if the item's description matches the desired value
+        filters_raw_data = response.json()
+        
+        # Iterate over the data from the current page of the filters API
+        for filters in filters_raw_data["result"]:
+
+            # Check if the filter matches the expression 
             if "ip.src in $crowdsec_managed_challenge" in filters["expression"]:
+                
                 # Get the ID from the current item
                 filters_id = filters["id"]
-            #    print(firewall_rule_id)
-                # Make a request to the third API endpoint for the current IDs
+
+                # Make a request to the filters API endpoint for the current IDs
                 third_api_url = BASE_URL + \
                     f"/{zone_id}/filters/{filters_id}"
                 response = requests.delete(third_api_url, headers=headers)
                 print(response.text)
-            #    print(data)
+
         # Check if there are more pages of results
         if not data["result"]:
             break
