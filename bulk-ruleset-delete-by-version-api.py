83db5783250520a11f16919b66521b053a1001b4
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-02-17 20:20:41 +0800

    added comments

--- bulk-ruleset-delete-by-version-api.py
+++ bulk-ruleset-delete-by-version-api.py
@@ -36,17 +36,19 @@ for zone_ids in data["result"]:
     for rulesets_ids in data["result"]:
         if rulesets_ids["phase"] == "http_request_firewall_custom":
             ruleset_id = rulesets_ids["id"]
-           
             
+            # Get versions objects
             rulesets_versions_api = BASE_URL + \
                 f"/{zone_id}/rulesets/{ruleset_id}/versions"
             response = requests.get(rulesets_versions_api, headers=headers)
             data = response.json()
           
+            # Iterate over the version ids and skipping the latest one, which is usually in the first object, since you can't delete the latest version as it is the same as the pinned version at this point         
             for rulesets_versions_id in data["result"][1:]:
                 for version_id in rulesets_versions_id["version"]:
                     version_id = rulesets_versions_id["version"]
                 
+                # Nuke it
                 rulesets_specific_versions_api = BASE_URL + \
                     f"/{zone_id}/rulesets/{ruleset_id}/versions/{version_id}"
                 response = requests.delete(rulesets_specific_versions_api, headers=headers)
