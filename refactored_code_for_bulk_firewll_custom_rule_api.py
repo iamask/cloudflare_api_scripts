91713476dc4534e9f3d09e46cc886b5a36acd25f
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-02-22 10:54:37 +0800

    fixing payload to intiate new custom ruleset phase

--- refactored_code_for_bulk_firewll_custom_rule_api.py
+++ refactored_code_for_bulk_firewll_custom_rule_api.py
@@ -114,11 +114,11 @@ def get_custom_ruleset_id(BASE_URL, headers):
 def intiate_custom_ruleset_for_new_zones(BASE_URL, headers):
     
     zone_ids = iterate_zone_ids_into_list(BASE_URL, headers)
-    new_custom_ruleset = { "name": "Default", "kind": "custom", "phase": "http_request_firewall_custom" }
-        
+    new_custom_ruleset = { "name": "Default", "kind": "zone", "source": "firewall_custom", "phase": "http_request_firewall_custom", "rules": [] }
+    
     for zone_id in zone_ids:
-        create_new_custom_ruleset = BASE_URL + f"/{zone_id}/rulesets"
-        response = requests.post(create_new_custom_ruleset, headers=headers, json=new_custom_ruleset)
+        create_new_custom_ruleset = BASE_URL + f"/{zone_id}/rulesets/phases"
+        response = requests.put(create_new_custom_ruleset, headers=headers, data=new_custom_ruleset)
         if response.status_code == 404:
             continue
         if response.status_code != 200:
@@ -155,8 +155,6 @@ def get_current_custom_ruleset_data(BASE_URL, headers):
                 custom_ruleset.append(current_custom_ruleset)
                     
         return custom_ruleset
-    
-print(get_current_custom_ruleset_data(BASE_URL, headers))
 
 def prepare_firewall_rules_for_migration(BASE_URL, headers):
     
